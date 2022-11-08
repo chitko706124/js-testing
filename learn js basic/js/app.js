85ce4cbbbe42948055ff670db89a80fbf8440f6c
chitkoko706124 <chitkoko124706@gmail.com>
2022-11-08 08:53:16 -0800

    first time

---
+++ learn js basic/js/app.js
@@ -0,0 +1,214 @@
+// function inchesToFeet(input){
+
+//     return input /12 + " ft";
+// }
+
+// console.log(inchesToFeet(50));
+// console.log(inchesToFeet(36));
+// console.log(inchesToFeet(79));
+// console.log(inchesToFeet(329.434));
+// console.log(inchesToFeet(846));
+
+
+//array
+
+// let fruits = ["apple","orange","banana","mango"];
+
+// resign value of index 2
+// fruits[2] = "aaa banana"
+
+// console.log(fruits)
+// console.log(fruits[2])
+
+
+// let mySelf = [
+//     "Chit Ko Ko", 
+//     "18",
+//     null,
+//     ["html","css","js"],
+//     "Thingangyun",
+//     10000000,
+//     false,
+//     "developer"
+// ]
+
+// console.log(mySelf)
+
+// console.log(`my name is ${mySelf[0]} and ${mySelf[1]} years old
+//     live in ${mySelf[4]}.
+//     I have ${mySelf[5]}.
+//     I have ${mySelf[2]}.
+//     Work as ${mySelf[7]} and understan in ${mySelf[3] [1]}.
+// `)
+
+
+// let arr = [];
+
+// //add new elements
+// arr [0] = "aaa"
+// arr [1] = "bbb"
+// arr [7] = "bbb"
+// arr [4] = "ccc"
+
+
+
+// console.log(typeof arr);
+// console.log(arr);
+// console.log(arr.length)
+
+// let arr = [];
+
+// arr[arr.length] = "aaa"
+// arr[arr.length] = "bbb"
+// arr[arr.length] = "ccc"
+// arr[arr.length] = "ddd"
+
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[arr.length - 1])
+
+//object 
+
+// let mySelf = {
+//     myNamek:"Chit Ko KO",
+//     myAge:18,
+//     gf:null,
+//     myJob:"web developer",
+//     skill:["html","css","js"],
+
+// };
+
+// console.log(typeof mySelf)
+// console.log(mySelf)
+// console.log(mySelf.myNamek)
+// console.log(mySelf.skill[1])
+// console.log(mySelf.skill)
+
+// console.log(mySelf["myJob"])
+
+
+// let obj = {};
+
+// obj.a = "chit ko ko";
+// obj.job = "web developer"
+
+
+// console.log(typeof obj)
+// console.log(obj)
+// console.log(obj.job)
+
+
+//problem
+
+// let name = "mg mg";
+// let age = 15;
+// let money = 200;
+
+// money -= 50; //bought orange
+
+// console.log(money)
+
+// let mgmgBag = ["mm","eng","bio",false];
+
+// let canMgMgSmoke = false;
+// let canMgMgFriendsSomke = true;
+
+// let mgmgObj = {
+//     name : "mg mg",
+//     age : 15,
+//     cansmoke : false,
+//     money,
+//     bag : mgmgBag,
+
+// }
+
+// let kyawkyawObj = {
+//     name : "kyaw kyaw",
+//     age : 16,
+//     cansmoke : true,
+//     money : 500,
+//     bag : ["mm"],
+    
+// }
+
+
+// let myamyaObj = {
+//     name : "mya mya",
+//     age : 15,
+//     cansmoke : true,
+//     money : 1000,
+//     bag : ["mm","eng","math","che","phy","bio"]
+    
+// }
+
+
+// console.log(mgmgObj)
+// console.log(kyawkyawObj)
+// console.log(myamyaObj)
+
+
+// const students = [
+//     {
+//         name : "mg mg",
+//         age : 15,
+//         cansmoke : false,
+//         money,
+//         bag : mgmgBag,
+    
+//     },
+
+//     {
+//         name : "kyaw kyaw",
+//         age : 16,
+//         cansmoke : true,
+//         money : 500,
+//         bag : ["mm"],
+        
+//     },
+
+//     {
+//         name : "mya mya",
+//         age : 15,
+//         cansmoke : true,
+//         money : 1000,
+//         bag : ["mm","eng","math","che","phy","bio"]
+        
+//     },
+
+
+// ];
+
+// console.log(students)
+// console.log(students[0].name)
+// console.log(students[2].bag[2])
+
+
+
+//let num = 5;
+
+// num = num + 1;
+// num = num + 1;
+// num = num + 1;
+// num = num + 1;
+
+// num += 1;
+// num += 1;
+// num += 1;
+
+// ++num;
+// --num;
+
+//console.log(++num)
+
+// logic operator && || !
+
+// console.log(!(5>10))
+
+// let condition = true
+
+// console.log(!condition)
+
+
+let condation = true || false
+
+console.log(condation)
